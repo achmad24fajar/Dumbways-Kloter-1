00fbc6cdf435fa96f3369f1f2d2c9454f59cecdd
achmadfajar-24 <achmad24fajar@gmail.com>
2021-02-03 14:37:13 +0700

    Menambahkan file 1.html dan 2.html dan menghapus soal no 4

--- Soal No.2/2.js
+++
@@ -1,7 +0,0 @@
-function hitungHurufDariKalimat(str, kalimat){
-	var regex = new RegExp(str, "g");
-	var hasil = kalimat.match(regex)
-	console.log(hasil.length)
-}
-
-hitungHurufDariKalimat('a', 'saya mau makan sate bersama teman saya setelah lulus dari sekolah dasar')
